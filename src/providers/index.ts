d657f7092c165540cf894cae0981790f806b28c6
Jan Kremen <jan@expan.do>
2018-09-29 13:19:54 +0200

    Changing npmrc config flow. Adding reexports. Adding documenting pre-commit githook

---
+++ src/providers/index.ts
@@ -0,0 +1,6 @@
+export * from './redirect-provider';
+export * from './value-provider';
+export * from './class-provider';
+export * from './providers-storage';
+export * from './provider-factory';
+export * from './provider';
